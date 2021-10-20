52d8a3a3619a42d30da22fc8aa3a47691b4aa5c1
Johanson Felix <felix.johanson@gmail.com>
2021-10-20 09:49:54 -0400

    added tooltips to tech logos on about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,7 +16,7 @@ export default function About() {
             My interest in Software/Web Development started when I was a curious
             teenager, wanting to be a scientist - to solve real world
             problems. My speciality is in building enterprise-level APIs using
-            the REST architecture. In my downtime I enjoy music, catching up on
+            the REST architecture. I also dabble in UI/UX design. In my downtime I enjoy music, catching up on
             my favorite Netflix drama series, meditating and trying new cuisine.
           </p>
           <div className="education">
@@ -32,28 +32,26 @@ export default function About() {
           </div>
           <div className="technologies">
             <p>Here are a few technologies I’ve been working with recently:</p> 
-              <i className="devicon-java-plain"></i>
-              <i className="devicon-spring-plain"></i>
-              <i className="devicon-html5-plain"></i>
-              <i className="devicon-css3-plain"></i>
-              <i className="devicon-sass-original"></i>
-              <i className="devicon-javascript-plain"></i>
-              <i className="devicon-react-original"></i>
-              <i className="devicon-nextjs-original"></i>
-              <i className="devicon-nodejs-plain-wordmark"></i>
-              <i className="devicon-express-original"></i>
-              <i className="devicon-python-plain"></i>
-              <i className="devicon-flask-original"></i>
-              <i className="devicon-django-plain"></i>
-              <i className="devicon-cplusplus-plain"></i>
-              <i className="devicon-mysql-plain"></i>
-              <i className="devicon-postgresql-plain"></i>
-              <i className="devicon-mongodb-plain"></i>
-              <i className="devicon-figma-plain"></i>
-              <i className="devicon-photoshop-plain"></i>
-              <i className="devicon-illustrator-plain"></i>
-
-
+              <i title="Java" className="devicon-java-plain"></i>
+              <i title="CSS3" className="devicon-css3-plain"></i>
+              <i title="HTML5" className="devicon-html5-plain"></i>
+              <i title="Spring Framework" className="devicon-spring-plain"></i>
+              <i title="Sass" className="devicon-sass-original"></i>
+              <i title="JavaScript" className="devicon-javascript-plain"></i>
+              <i title="React" className="devicon-react-original"></i>
+              <i title="Next.js" className="devicon-nextjs-original"></i>
+              <i title="Node.js" className="devicon-nodejs-plain-wordmark"></i>
+              <i title="Express.js" className="devicon-express-original"></i>
+              <i title="Python" className="devicon-python-plain"></i>
+              <i title="Flask" className="devicon-flask-original"></i>
+              <i title="Django" className="devicon-django-plain"></i>
+              <i title="C++" className="devicon-cplusplus-plain"></i>
+              <i title="Mysql" className="devicon-mysql-plain"></i>
+              <i title="PostgreSQL" className="devicon-postgresql-plain"></i>
+              <i title="MongoDB" className="devicon-mongodb-plain"></i>
+              <i title="Figma" className="devicon-figma-plain"></i>
+              <i title="Adobe Photoshop" className="devicon-photoshop-plain"></i>
+              <i title="Adobe Illustrator" className="devicon-illustrator-plain"></i>
            
           </div>
         </div>
